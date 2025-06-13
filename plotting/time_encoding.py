d8a34084d34ea05a4594b42511cc2ba6972f64fd
Caputommy <caputommy@gmail.com>
2025-06-13 20:53:49 +0200

    Added file for plotting time performance comparisons

--- plotting/time_encoding.py
+++ plotting/time_encoding.py
@@ -4,8 +4,8 @@ import numpy as np
 def plot_classification_times():
     # Data
     models = ['VotingEnsemble', 'BaggedNN', 'RandomForest']
-    encoded_times = [0.000041, 0.000032, 0.000011]
-    not_encoded_times = [0.000035, 0.000033, 0.000003]
+    encoded_times = [41, 32, 11]
+    not_encoded_times = [35, 33, 3]
 
     # Colors
     light_red = "#FFE4E4"
@@ -24,7 +24,7 @@ def plot_classification_times():
                    color=light_red, edgecolor=dark_red)
 
     # Labels and title
-    ax.set_ylabel('Average Classification Time (seconds)')
+    ax.set_ylabel('Average Classification Time (microseconds)')
     ax.set_title('Average Time per Record: Encoded vs Not Encoded')
     ax.set_xticks(x)
     ax.set_xticklabels(models)
@@ -34,7 +34,7 @@ def plot_classification_times():
     for bars in [bars1, bars2]:
         for bar in bars:
             height = bar.get_height()
-            ax.annotate(f'{height:.1e}',
+            ax.annotate(f'{height}',
                         xy=(bar.get_x() + bar.get_width() / 2, height),
                         xytext=(0, 3),
                         textcoords="offset points",
